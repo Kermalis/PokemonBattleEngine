f69592c474b647543880a346a06bb1df80bb6af2
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-10-13 23:13:03 -0400

    Oops, forgot this all along

---
+++ PokemonBattleEngine/Battle.cs
@@ -0,0 +1,20 @@
+﻿using PokemonBattleEngine.Data;
+
+namespace PokemonBattleEngine
+{
+    class Team
+    {
+        public Pokemon[] Pokemon = new Pokemon[Constants.MaxPokemon];
+        public string PlayerName;
+    }
+
+    class Battle
+    {
+        readonly Team[] teams;
+
+        public Battle(Team[] teams)
+        {
+            this.teams = teams;
+        }
+    }
+}
