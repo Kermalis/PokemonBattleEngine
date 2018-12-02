202e4d7f26a9771b46e231717a12c3f3bda4b044
Tulu473 <39826439+Tulu473@users.noreply.github.com>
2018-12-02 09:38:58 -0300

    Misdreavus

--- PokemonBattleEngine/Data/Enums.cs
+++ PokemonBattleEngine/Data/Enums.cs
@@ -400,6 +400,7 @@ namespace Kermalis.PokemonBattleEngine.Data
         Crobat = 169,
         Pichu = 172,
         Azumarill = 184,
+        Misdreavus = 200,
         Unown_A = 201 | (0 << 0x10),
         Unown_B = 201 | (1 << 0x10),
         Unown_C = 201 | (2 << 0x10),
