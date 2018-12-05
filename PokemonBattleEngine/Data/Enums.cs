17baa20be7e8dbbaed0fa307bdad3727be23bdd1
tulu473 <tuku473@gmail.com>
2018-12-05 03:19:29 -0300

    Umbreon

--- PokemonBattleEngine/Data/Enums.cs
+++ PokemonBattleEngine/Data/Enums.cs
@@ -531,6 +531,7 @@ namespace Kermalis.PokemonBattleEngine.Data
         Pichu = 172,
         Azumarill = 184,
         Espeon = 196,
+        Umbreon = 197,
         Misdreavus = 200,
         Unown_A = 201 | (0 << 0x10),
         Unown_B = 201 | (1 << 0x10),
