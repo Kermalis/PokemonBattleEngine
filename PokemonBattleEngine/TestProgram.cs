b6f2fd5e96a96adbcac15d6377d7ef09a9773bff
Kermalis <29823718+Kermalis@users.noreply.github.com>
2018-12-11 23:46:12 -0500

    Use a log file for TestProgram.cs

--- PokemonBattleEngine/TestProgram.cs
+++ PokemonBattleEngine/TestProgram.cs
@@ -1,21 +1,37 @@
 ﻿using Kermalis.PokemonBattleEngine.Battle;
 using Kermalis.PokemonBattleEngine.Data;
 using System;
+using System.IO;
 using System.Linq;
 
 namespace Kermalis.PokemonBattleEngine
 {
     class PBETestProgram
     {
+        static readonly string logFile = "Test Log.txt";
+        static StreamWriter writer;
+        static TextWriter oldWriter;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Pokémon Battle Engine Test");
-            Console.WriteLine();
+            Console.WriteLine("----- Pokémon Battle Engine Test -----");
 
             PBESettings settings = PBESettings.DefaultSettings;
             PBEBattle battle = new PBEBattle(PBEBattleFormat.Single, settings, PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize), PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize));
             battle.OnNewEvent += PBEBattle.ConsoleBattleEventHandler;
             battle.OnStateChanged += Battle_OnStateChanged;
+            try
+            {
+                writer = new StreamWriter(new FileStream(logFile, FileMode.Create, FileAccess.Write));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Cannot open \"{logFile}\" for writing.");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            oldWriter = Console.Out;
+            Console.SetOut(writer);
             battle.Begin();
         }
         static void Battle_OnStateChanged(PBEBattle battle)
@@ -23,11 +39,14 @@ namespace Kermalis.PokemonBattleEngine
             switch (battle.BattleState)
             {
                 case PBEBattleState.Ended:
+                    Console.SetOut(oldWriter);
+                    writer.Close();
+                    Console.WriteLine($"Test battle ended. \"{logFile}\" contains the battle.");
                     Console.ReadKey();
                     break;
                 case PBEBattleState.ReadyToRunTurn:
                     Console.WriteLine();
-                    foreach (PBEPokemon pkmn in battle.ActiveBattlers)
+                    foreach (PBEPokemon pkmn in battle.Teams[0].ActiveBattlers.Concat(battle.Teams[1].ActiveBattlers))
                     {
                         Console.WriteLine(pkmn);
                         Console.WriteLine();
