a875505307bb80ca0d7e1bb220bcb6e68e777b68
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-01-05 03:33:22 -0500

    Japanese Pokémon names

--- PokemonBattleEngine/Localization/LocalizedString.cs
+++ PokemonBattleEngine/Localization/LocalizedString.cs
@@ -5,15 +5,18 @@ namespace Kermalis.PokemonBattleEngine.Localization
     public sealed class PBELocalizedString
     {
         public string English { get; }
+        public string Japanese { get; }
 
-        internal PBELocalizedString(string english)
+        internal PBELocalizedString(string english, string japanese)
         {
             English = english;
+            Japanese = japanese;
         }
 
         public bool Contains(string str, StringComparison comparisonType = StringComparison.InvariantCultureIgnoreCase)
         {
-            if (English.Equals(str, comparisonType))
+            if (English.Equals(str, comparisonType)
+                || Japanese.Equals(str, comparisonType))
             {
                 return true;
             }
