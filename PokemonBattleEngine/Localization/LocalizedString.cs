493ca396a61443aef8866e7c0bda3c1b9dfa8414
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-01-09 01:04:28 -0500

    Korean, Italian, Spanish, German, and French ability, item, move, and Pokémon names

--- PokemonBattleEngine/Localization/LocalizedString.cs
+++ PokemonBattleEngine/Localization/LocalizedString.cs
@@ -5,18 +5,33 @@ namespace Kermalis.PokemonBattleEngine.Localization
     public sealed class PBELocalizedString
     {
         public string English { get; }
+        public string French { get; }
+        public string German { get; }
+        public string Italian { get; }
         public string Japanese { get; }
+        public string Korean { get; }
+        public string Spanish { get; }
 
-        internal PBELocalizedString(string english, string japanese)
+        internal PBELocalizedString(string japanese, string korean, string french, string german, string spanish, string italian, string english)
         {
             English = english;
+            French = french;
+            German = german;
+            Italian = italian;
             Japanese = japanese;
+            Korean = korean;
+            Spanish = spanish;
         }
 
         public bool Contains(string str, StringComparison comparisonType = StringComparison.InvariantCultureIgnoreCase)
         {
             if (English.Equals(str, comparisonType)
-                || Japanese.Equals(str, comparisonType))
+                || French.Equals(str, comparisonType)
+                || German.Equals(str, comparisonType)
+                || Italian.Equals(str, comparisonType)
+                || Japanese.Equals(str, comparisonType)
+                || Korean.Equals(str, comparisonType)
+                || Spanish.Equals(str, comparisonType))
             {
                 return true;
             }
