d117a7e2ad936a62be8584f8381cc61163390459
tulu473 <tuku473@gmail.com>
2019-01-16 09:16:52 -0300

    Ferrothorn

--- PokemonBattleEngine/Data/Enums.cs
+++ PokemonBattleEngine/Data/Enums.cs
@@ -2228,6 +2228,7 @@ namespace Kermalis.PokemonBattleEngine.Data
         Empoleon = 395,
         Espeon = 196,
         Farfetchd = 83,
+        Ferrothorn = 598,
         Flareon = 136,
         Gabite = 444,
         Garchomp = 445,
