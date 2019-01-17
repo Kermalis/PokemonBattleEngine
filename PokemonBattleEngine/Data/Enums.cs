d91f9a77f827dafaa4c7517cd286a3dd59eb5a67
tulu473 <tuku473@gmail.com>
2019-01-16 23:12:02 -0300

    Thundurus

--- PokemonBattleEngine/Data/Enums.cs
+++ PokemonBattleEngine/Data/Enums.cs
@@ -2318,6 +2318,8 @@ namespace Kermalis.PokemonBattleEngine.Data
         Snivy = 495,
         Squirtle = 7,
         Tepig = 498,
+        Thundurus = 642 | (0 << 0x10),
+        Thundurus_Therian = 642 | (1 < 0x10),
         Tirtouga = 564,
         Torchic = 255,
         Torterra = 389,
--- PokemonBattleEngine/Localization/LocalizationGenerator.cs
+++ PokemonBattleEngine/Localization/LocalizationGenerator.cs
@@ -406,6 +406,7 @@ namespace Kermalis.PokemonBattleEngine.Localization
                     case PBESpecies.Giratina: numForms = 2; break;
                     case PBESpecies.Landorus: numForms = 2; break;
                     case PBESpecies.Rotom: numForms = 6; break;
+                    case PBESpecies.Thundurus: numForms = 2; break;
                     case PBESpecies.Unown_A: numForms = 28; break;
                     default: numForms = 1; break;
                 }
