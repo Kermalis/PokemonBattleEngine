f37fa312100a35512e434860f9b4e332ae57f054
Kermalis <29823718+Kermalis@users.noreply.github.com>
2019-03-17 19:06:38 -0400

    Add completely randomized Pokémon to AI battle test

--- PokemonBattleEngineTesting/AIBattleTest.cs
+++ PokemonBattleEngineTesting/AIBattleTest.cs
@@ -2,6 +2,7 @@
 using Kermalis.PokemonBattleEngine.AI;
 using Kermalis.PokemonBattleEngine.Battle;
 using Kermalis.PokemonBattleEngine.Data;
+using Kermalis.PokemonBattleEngine.Localization;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -21,9 +22,61 @@ namespace Kermalis.PokemonBattleEngineTesting
             Console.WriteLine("----- Pokémon Battle Engine Test -----");
 
             PBESettings settings = PBESettings.DefaultSettings;
-            PBEPokemonShell[] team0Party = PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize).ToArray();
-            PBEPokemonShell[] team1Party = PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize).ToArray();
-            /*PBEPokemonShell[] team0Party = new PBEPokemonShell[]
+            PBEPokemonShell[] team0Party, team1Party;
+
+            // Completely Randomized Pokémon
+            team0Party = new PBEPokemonShell[settings.MaxPartySize];
+            team1Party = new PBEPokemonShell[settings.MaxPartySize];
+            IEnumerable<PBESpecies> allSpecies = Enum.GetValues(typeof(PBESpecies)).Cast<PBESpecies>().Except(new[] { PBESpecies.Arceus_Bug, PBESpecies.Arceus_Dark, PBESpecies.Arceus_Dragon, PBESpecies.Arceus_Electric, PBESpecies.Arceus_Fighting, PBESpecies.Arceus_Fire, PBESpecies.Arceus_Flying, PBESpecies.Arceus_Ghost, PBESpecies.Arceus_Grass, PBESpecies.Arceus_Ground, PBESpecies.Arceus_Ice, PBESpecies.Arceus_Poison, PBESpecies.Arceus_Psychic, PBESpecies.Arceus_Rock, PBESpecies.Arceus_Steel, PBESpecies.Arceus_Water, PBESpecies.Castform_Rainy, PBESpecies.Castform_Snowy, PBESpecies.Castform_Sunny, PBESpecies.Cherrim_Sunshine, PBESpecies.Darmanitan_Zen, PBESpecies.Genesect_Burn, PBESpecies.Genesect_Chill, PBESpecies.Genesect_Douse, PBESpecies.Genesect_Shock, PBESpecies.Giratina_Origin, PBESpecies.Keldeo_Resolute, PBESpecies.Meloetta_Pirouette });
+            for (int i = 0; i < settings.MaxPartySize; i++)
+            {
+                PBEPokemonShell Create()
+                {
+                    PBESpecies species = allSpecies.Sample();
+                    PBEPokemonData pData = PBEPokemonData.Data[species];
+                    var shell = new PBEPokemonShell
+                    {
+                        Species = species,
+                        Ability = pData.Abilities.Sample(),
+                        Gender = PBEUtils.RNG.NextGender(species),
+                        Level = settings.MaxLevel,
+                        Friendship = byte.MaxValue,
+                        Nature = (PBENature)PBEUtils.RNG.Next((int)PBENature.MAX),
+                        Nickname = PBEPokemonLocalization.Names[(PBESpecies)((uint)species & 0xFFFF)].English,
+                        Shiny = PBEUtils.RNG.NextShiny(),
+                        EVs = new byte[6],
+                        IVs = new byte[6],
+                        PPUps = new byte[settings.NumMoves],
+                        Moves = new PBEMove[settings.NumMoves],
+                        Item = PBEItem.None
+                    };
+                    for (int j = 0; j < 6; j++)
+                    {
+                        shell.IVs[j] = (byte)PBEUtils.RNG.Next(settings.MaxIVs + 1);
+                    }
+                    var legalMoves = PBELegalityChecker.GetLegalMoves(species, shell.Level).ToList();
+                    for (int j = 0; j < settings.NumMoves; j++)
+                    {
+                        if (legalMoves.Count == 0)
+                        {
+                            break;
+                        }
+                        PBEMove move = legalMoves.Sample();
+                        shell.Moves[j] = move;
+                        legalMoves.Remove(move);
+                    }
+                    return shell;
+                }
+                team0Party[i] = Create();
+                team1Party[i] = Create();
+            }
+
+            // Randomized Competitive Pokémon
+            /*team0Party = PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize).ToArray();
+            team1Party = PBECompetitivePokemonShells.CreateRandomTeam(settings.MaxPartySize).ToArray();*/
+
+            // Predefined Pokémon
+            /*team0Party = new PBEPokemonShell[]
             {
                 PBECompetitivePokemonShells.Zoroark_VGC,
                 PBECompetitivePokemonShells.Volcarona_VGC,
@@ -32,7 +85,7 @@ namespace Kermalis.PokemonBattleEngineTesting
                 PBECompetitivePokemonShells.Vanilluxe_VGC,
                 PBECompetitivePokemonShells.Chandelure_VGC
             };
-            PBEPokemonShell[] team1Party = new PBEPokemonShell[]
+            team1Party = new PBEPokemonShell[]
             {
                 PBECompetitivePokemonShells.Arceus_Uber,
                 PBECompetitivePokemonShells.Darkrai_Uber,
@@ -41,6 +94,7 @@ namespace Kermalis.PokemonBattleEngineTesting
                 PBECompetitivePokemonShells.Metagross_VGC,
                 PBECompetitivePokemonShells.Victini_Uber
             };*/
+
             var battle = new PBEBattle(PBEBattleFormat.Double, settings, team0Party, team1Party);
             battle.Teams[0].TrainerName = "Team 1";
             battle.Teams[1].TrainerName = "Team 2";
